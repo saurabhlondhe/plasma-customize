6d2dd2b9780a4504f7727af6550927a48a18d396
Saurabh Londhe <saurabhlondhe1111@gmail.com>
2017-09-22 12:10:27 -0400

    Update Splash.py

--- Splash.py
+++ Splash.py
@@ -1,11 +1,14 @@
 import os
 import commands
 status, output = commands.getstatusoutput("locate -b '\splash'")
-print output
-cd="cd "+output
-os.system(cd+" && rm Splash.qml")
-os.system(cd+" && wget https://raw.githubusercontent.com/saurabhlondhe/plasma-customize/master/Splash.qml")
+lines = output.split('\n')
+data=[]
+for line in lines:
+    data.append(line)
+cd="cd "+data[0]
+os.system(cd+" && sudo rm Splash.qml")
+os.system(cd+" && sudo wget https://raw.githubusercontent.com/saurabhlondhe/plasma-customize/master/Splash.qml")
 cd_images=cd+"/images && "
-os.system(cd_images+"wget https://raw.githubusercontent.com/saurabhlondhe/plasma-customize/master/images/1234.jpg")
-os.system(cd_images+"wget https://raw.githubusercontent.com/saurabhlondhe/plasma-customize/master/images/logo.png")
-os.system(cd_images+"wget https://raw.githubusercontent.com/saurabhlondhe/plasma-customize/master/images/8iEknLaia.png")
+os.system(cd_images+"sudo wget https://raw.githubusercontent.com/saurabhlondhe/plasma-customize/master/images/1234.jpg")
+os.system(cd_images+"sudo wget https://raw.githubusercontent.com/saurabhlondhe/plasma-customize/master/images/logo.png")
+os.system(cd_images+"sudo wget https://raw.githubusercontent.com/saurabhlondhe/plasma-customize/master/images/8iEknLaia.png")
